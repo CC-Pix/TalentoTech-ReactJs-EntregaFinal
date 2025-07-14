c52ae230737f83428a275d03bf56f2a80225a28b
MarianaGil68 <marianagil@gmail.com>
2025-07-13 21:19:45 -0300

    Version 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
-import { CarritoProvider } from './context/CarritoContext.jsx';
+import { CarritoProvider } from './context/carritoContext.jsx';
 import './index.css';
 
 
